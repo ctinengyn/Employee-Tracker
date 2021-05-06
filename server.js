5adb23ae22be7665c7e9ae50d2266292d0370036
Christine Nguyen <ctine.ngyn@gmail.com>
2021-05-05 22:02:44 -0700

    Connection properties

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 require("console.table");
 
+// Connection Properties
+const connectionProperties = {
+    host: "localhost",
+    port: 1996,
+    user: "root",
+    password: "password",
+    database: "employees_DB"
+}
+
 // Initial Prompt 
 const startApp = () => {
     inquirer.prompt({
