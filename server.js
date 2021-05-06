a8319c6ae60810dfcfcaea3598865b48f93983ac
Christine Nguyen <ctine.ngyn@gmail.com>
2021-05-05 22:01:23 -0700

    Initial prompt

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 require("console.table");
+
+// Initial Prompt 
+const startApp = () => {
+    inquirer.prompt({
+        name: 'menuChoice',
+        type: 'list',
+        message: 'Select an option',
+        choices: startScreen
+    }
+}
