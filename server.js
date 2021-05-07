c10614c1125c3132bfc6eacfc7bd29155a4cb64b
Christine Nguyen <ctine.ngyn@gmail.com>
2021-05-06 23:32:49 -0700

    View all employess

--- server.js
+++ server.js
@@ -5,66 +5,78 @@ require("console.table");
 
 // Connection Properties
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 1996,
-    user: "root",
-    password: "password",
-    database: "employees_DB"
+  host: "localhost",
+  port: 1996,
+  user: "root",
+  password: "password",
+  database: "employees_DB",
 });
 
 // Establishing Connection to database
-connection.connect(function(err) {
-    if (err) throw err
-    console.log("Connected as Id" + connection.threadId)
-    startPrompt();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected as Id" + connection.threadId);
+  startPrompt();
 });
 
 // Prompt user to choose an option
 const startPrompt = () => {
-    inquirer.prompt({
-        name: "choice",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "View All Employees",
-            "View All Employees By Roles",
-            "View All Employees By Departments",
-            "Update Employee",
-            "Add Employee",
-            "Add Role",
-            "Add Department"
-        ]
-
-    }).then((answer) => {
-        // Switch case depending on user option
-        switch (answer.choice) {
-            case "View All Employees":
-                viewAllEmployees();
-                break;
-
-            case "View All Employees By Roles":
-                viewAllRoles();
-                break;
-
-            case "View All Employees By Departments":
-                viewAllDepartments();
-                break;
-
-            case "Add Employee":
-                addEmployee();
-                break;
-
-            case "Update Employee":
-                updateEmployee();
-                break;
-
-            case "Add Role":
-                addRole();
-                break;
-
-            case "Add Department":
-                addDepartment();
-                break;
-        }
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Roles",
+        "View All Employees By Departments",
+        "Update Employee",
+        "Add Employee",
+        "Add Role",
+        "Add Department",
+      ],
     })
+    .then((answer) => {
+      // Switch case depending on user option
+      switch (answer.choice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "View All Employees By Roles":
+          viewAllRoles();
+          break;
+
+        case "View All Employees By Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
 };
+
+function viewAllEmployees() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
+}
