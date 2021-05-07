e0ab437ee6e2ac5f4c37ffad0a11fc3ed1cae4ea
Christine Nguyen <ctine.ngyn@gmail.com>
2021-05-06 23:07:01 -0700

    Connection to database

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+// Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 require("console.table");
 
 // Connection Properties
-const connectionProperties = {
+const connection = mysql.createConnection({
     host: "localhost",
     port: 1996,
     user: "root",
     password: "password",
     database: "employees_DB"
-}
+});
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startPrompt();
+});
+
 
 // Initial Prompt 
 const startApp = () => {
@@ -19,4 +27,4 @@ const startApp = () => {
         message: 'Select an option',
         choices: startScreen
     }
-}
+};
