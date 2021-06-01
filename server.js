0cdfee993cc6c918acc93ce1d0bc982e26053669
Christine Nguyen <ctine.ngyn@gmail.com>
2021-05-31 22:54:52 -0700

    Refactor

--- server.js
+++ server.js
@@ -33,9 +33,8 @@ const startPrompt = () => {
         "Add Employee",
         "Add Role",
         "Add Department",
-      ],
-    })
-    .then((answer) => {
+      ]
+    }).then((answer) => {
       // Switch case depending on user option
       switch (answer.choice) {
         case "View All Employees":
@@ -72,6 +71,8 @@ const startPrompt = () => {
 // View all employess
 function viewAllEmployees() {
     
+  var query = "SELECT * FROM employees";
+
     // Query to view all employees
   connection.query(
     "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
@@ -83,7 +84,7 @@ function viewAllEmployees() {
       // Display query results using console.table
       console.table(res);
       startPrompt();
-  )
+    })
 
   // View all employees by role
   function viewAllRoles() {
